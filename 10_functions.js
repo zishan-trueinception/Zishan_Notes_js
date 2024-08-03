1151ef62a33c9dd1177240532f9d0b37d36b956c
Zishan Shaikh <zishan@truelink.ai/in>
2024-08-03 15:01:34 +0530

    functions

--- 10_functions.js
+++ 10_functions.js
@@ -6,7 +6,7 @@ function myFunction() {
 // myFunction =>  reference of function
 // myFunction() =>  call of function
 
-myFunction()
+myFunction()    // o/p hello world
 
 // two numbers addition by function
 
@@ -20,7 +20,7 @@ function substraction(number1, number2) {
     const result = number1 - number2
     console.log(result);
 }
-substraction(20,10)
+substraction(20,10) // o/p 10
 
 
 
@@ -28,20 +28,19 @@ function addition(number1, number2) {
     console.log(number1 + number2);
     
 }
-
-addition(10, 20)
+addition(10, 20)    // o/p 30
 
 
 function loginUserMessage(username) {
     return `Welcome ${username} just logged in`
     
 }
-console.log(loginUserMessage("zishan"));
+console.log(loginUserMessage("zishan"));    // o/p Welcome zishan just logged in
 
 // rest 
 function calculateCardPrice(...num1){
     return num1
     
 }
-console.log(calculateCardPrice(100,200,500,1000,2500));
+console.log(calculateCardPrice(100,200,500,1000,2500)); // o/p [ 100, 200, 500, 1000, 2500 ]
 
