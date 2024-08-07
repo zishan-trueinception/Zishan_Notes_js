cf1f2aad2572155ff04b590187e66802e7542b6a
Zishan Shaikh <zishan@truelink.ai/in>
2024-08-07 13:20:42 +0530

    new changes

--- 14_if.js
+++ 14_if.js
@@ -16,32 +16,33 @@ if (false) {
 */
 
 const temperature = 30
-// if(temperature < 50){
-//     console.log("Temperature is less than 50");
+if(temperature < 50){
+    console.log("Temperature is less than 50");
     
-// }else{
-//     console.log("Temperature is greater than 50");
+}else{
+    console.log("Temperature is greater than 50");
 
-// }
+}
 
 const score = 200
 
 if(score > 100){
     let power = "fly"
-    // console.log(`User Power ${power}`); // o/p User Power fly
+    console.log(`User Power ${power}`); // o/p User Power fly
     
 }
-// console.log(`User Power : ${power}`);   // o/p Uncaught ReferenceError: power is not defined
+console.log(`User Power : ${power}`);   // o/p Uncaught ReferenceError: power is not defined
 
-// const balance = 700
-// if(balance < 700){
-//     console.log("User has less balance");
-// }else if(balance > 700){
-//     console.log("User has extra balance");
-// }else if(balance==700){
-//     console.log("User has sufficient balance");
-// }
+const balance = 700
+if(balance < 700){
+    console.log("User has less balance");
+}else if(balance > 700){
+    console.log("User has extra balance");
+}else if(balance==700){
+    console.log("User has sufficient balance");
+}
 
+// in && operator if one condition is false it will return false or else it will return true
 const userLogedin = true
 const debitCard = true
 if(userLogedin && debitCard){
@@ -54,16 +55,19 @@ if(userLogedin && debitCard){
 
 const userLogedIn = true
 const userhasDebitCard = false
+
 if(userLogedin && userhasDebitCard){
     console.log("User can make payment");
 }else{
     console.log("User cannot make payment");
 }
+
 // o/p User cannot make payment
 
-
+// in || operator if one condition is true it will return true or else it will return false
 const loggedinFromGoogle = true
 const loggedinFromEmail = true
+
 if(loggedinFromGoogle || loggedinFromEmail){
     console.log("User can use google or email");
 }else{
