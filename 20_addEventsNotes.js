65a33f8964dc6884348ae75356563c26f14c3605
Zishan Shaikh <zishan@truelink.ai/in>
2024-08-09 19:23:10 +0530

    types of events notes js

---
+++ 20_addEventsNotes.js
@@ -0,0 +1,24 @@
+/*
+
+types of events
+    1. click
+    2. mouseover
+    3. mouseout
+    4. mouseenter
+    5. mouseleave
+    6. keyup
+    7. keyboard type events mouse events
+
+    8.timestamps  => times date activity
+    9.target => use to get the target of the event
+    10.tooElement => use to get the element of the event
+    11.preventDefault => use to stop default action of any event
+    12.srcElemnet =>
+    13.currentTarget => 
+
+
+
+
+
+
+*/
