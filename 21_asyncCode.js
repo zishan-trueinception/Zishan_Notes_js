a73ebdb47c0c0f05af6e85465a50c9969d272c16
Zishan Shaikh <zishan@truelink.ai/in>
2024-08-10 12:49:38 +0530

    synchronus notes js

---
+++ 21_asyncCode.js
@@ -0,0 +1,18 @@
+/*
+-javaScript is Synchronous(Default javaScript)
+    -step by step execution performed
+-single threaded language 
+-Execution Context
+    -executio one line of code at a time
+    (each operation waits for the last one to compile before executing the next line)
+
+-BlokingCode  
+    -block the flow of the program 
+     -read file sync 
+     
+-Non Blocking Code 
+    -Read filr async
+
+    reference code html file => interval.html
+    
+*/
